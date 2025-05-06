c97cca2a44fa7d14a6236bd24b6fd53a1237278c
dvieira13 <vieiradylan13@gmail.com>
2025-05-06 09:09:45 -0400

    directories

--- public/components/nav.ts
+++ public/components/nav.ts
@@ -13,7 +13,7 @@ export function load_nav(containerId: string = "nav-placeholder"): void {
           <div class="nav-menu-option expandable-option">
             <div class="expandable-header">
               <p class="nav-menu-copy">More</p>
-              <img class="nav-expand-icon" src="./src/assets/icons/show.svg" />
+              <img class="nav-expand-icon" src="./assets/icons/show.svg" />
             </div>
             <div class="expandable-body">
               <a class="nav-menu-option" href="/pace-calculator" title="Pace Calculator">
@@ -26,7 +26,7 @@ export function load_nav(containerId: string = "nav-placeholder"): void {
           </div>
         </div>
         <a class="nav-logo" href="/" title="trainr home">
-          <img class="nav-logo-img" src="./src/assets/icons/trainr.svg" alt="trainr logo" />
+          <img class="nav-logo-img" src="./assets/icons/trainr.svg" alt="trainr logo" />
         </a>
         <div class="nav-right-container">
           <a class="nav-cta active-nav-account-button" id="nav_sign_in_button" href="/sign-in">
@@ -39,10 +39,10 @@ export function load_nav(containerId: string = "nav-placeholder"): void {
       </div>
       <div class="nav-mobile">
         <a class="nav-logo" href="/" title="trainr home">
-          <img class="nav-logo-img" src="./src/assets/icons/trainr.svg" alt="trainr logo" />
+          <img class="nav-logo-img" src="./assets/icons/trainr.svg" alt="trainr logo" />
         </a>
         <div class="nav-mobile-button">
-          <img class="nav-open-icon" src="./src/assets/icons/burger.svg" title="open nav">
+          <img class="nav-open-icon" src="./assets/icons/burger.svg" title="open nav">
         </div>
       </div>
     `;
